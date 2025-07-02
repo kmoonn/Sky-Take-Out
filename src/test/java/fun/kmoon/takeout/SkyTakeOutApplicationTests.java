58e304858655c8039a40a6f34e0f570fc90e941e
kmoonn <hushan@kmoon.fun>
2025-07-02 13:27:22 +0800

    项目初始化

---
+++ src/test/java/fun/kmoon/takeout/SkyTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.kmoon.takeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
